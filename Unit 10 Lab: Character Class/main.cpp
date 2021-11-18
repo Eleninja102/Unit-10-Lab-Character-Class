da0c73b78f3be8ee00b7e44ced7f50e42d5dff8c
Coleton Watt <electricninja102@gmail.com>
2021-11-18 10:32:02 -0700

    Initial Commit

---
+++ Unit 10 Lab: Character Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 10 Lab: Character Class
+//
+//  Created by Coleton Watt on 11/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
